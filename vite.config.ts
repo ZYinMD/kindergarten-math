15b566d653526ff7288db54f3c08ce36559c1f1d
Z Yin <Z.Yin.MD@gmail.com>
2023-03-27 16:08:18 -0400

    vite config: add ports

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-})
+  server: {
+    port: 5510,
+    strictPort: true,
+    open: true,
+  },
+  preview: {
+    port: 6612,
+    strictPort: true,
+    open: true,
+  },
+});
