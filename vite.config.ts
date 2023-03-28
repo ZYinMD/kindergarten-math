d603b89240fbd35f4deb84b884777060d11dc82e
Z Yin <Z.Yin.MD@gmail.com>
2023-03-28 01:01:22 -0400

    add vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,14 @@
-import { defineConfig } from "vite";
-import { svelte } from "@sveltejs/vite-plugin-svelte";
+/// <reference types="vitest" />
+
+import { defineConfig } from 'vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  test: {
+    includeSource: ['src/**/*.ts'],
+  },
   server: {
     port: 6636,
     strictPort: true,
