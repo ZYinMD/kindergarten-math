03486318d380b0d3e37fe979becd9138d6c10fb7
Z Yin <Z.Yin.MD@gmail.com>
2023-04-02 20:03:47 -0400

    v

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,9 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  build: {
+    target: 'safari12',
+  },
   test: {
     includeSource: ['src/**/*.ts'],
   },
