50ba4eb06c590d26c8d29239dd2018f17d4ad8f2
Z Yin <Z.Yin.MD@gmail.com>
2023-04-04 02:02:35 -0400

    refactor: f2 only

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
   import Game from './lib/Game/Game.svelte';
-  import GameSettings from './lib/GameSettings/GameSettings.svelte';
+  import Settings from './lib/Settings/Settings.svelte';
   import { currentView } from './lib/stores/$currentView';
 </script>
 
 {#if $currentView === 'gameSettings'}
-  <GameSettings />
+  <Settings />
 {/if}
 
 {#if $currentView === 'questions'}
