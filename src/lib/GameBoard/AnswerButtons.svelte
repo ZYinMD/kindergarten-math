6ac36a882df4fc5aafc31fc400c387884b2899e8
Z Yin <Z.Yin.MD@gmail.com>
2023-04-04 13:38:55 -0400

    refactor: extract function only

--- src/lib/GameBoard/AnswerButtons.svelte
+++ src/lib/GameBoard/AnswerButtons.svelte
@@ -6,26 +6,30 @@
   $: buttonArray = Array(numButtons)
     .fill(null)
     .map((item, index) => index + 1);
+
+  function handleClickAnswerButton(buttonNumber: number) {
+    const { question, type } = $currentQuestion;
+    let answer = 0;
+    if (type[0] === '_') answer = question[0];
+    if (type[2] === '_') answer = question[1];
+    if (type[4] === '_') answer = question[2];
+    if (buttonNumber === answer) {
+      correctCounter.update((prev) => prev + 1);
+    } else {
+      correctCounter.update((prev) => {
+        if (prev > 0) return prev - 1;
+        return prev;
+      });
+    }
+    questionUtil.next();
+  }
 </script>
 
 <section>
   {#each buttonArray as buttonNumber (buttonNumber)}
     <button
       on:click={() => {
-        const { question, type } = $currentQuestion;
-        let answer = 0;
-        if (type[0] === '_') answer = question[0];
-        if (type[2] === '_') answer = question[1];
-        if (type[4] === '_') answer = question[2];
-        if (buttonNumber === answer) {
-          correctCounter.update((prev) => prev + 1);
-        } else {
-          correctCounter.update((prev) => {
-            if (prev > 0) return prev - 1;
-            return prev;
-          });
-        }
-        questionUtil.next();
+        handleClickAnswerButton(buttonNumber);
       }}
     >
       {buttonNumber}
