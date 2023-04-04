2cf5ff914705eecff1b9861367c20d8f137ff8f8
Z Yin <Z.Yin.MD@gmail.com>
2023-04-04 12:36:00 -0400

    mvp of answer buttons

--- src/lib/Game/AnswerButtons.svelte
+++ src/lib/Game/AnswerButtons.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import { settings } from '../GameSettings/$settings';
+  import { currentQuestion } from '../questions/$progress';
 
   $: numButtons = Math.ceil($settings.maxValueAllowed / 10) * 10;
   $: buttonArray = Array(numButtons)
@@ -8,13 +9,22 @@
 </script>
 
 <section>
-  {#each buttonArray as button (button)}
+  {#each buttonArray as buttonNumber (buttonNumber)}
     <button
       on:click={() => {
-        console.log(`button ${button} clicked`);
+        const { question, type } = $currentQuestion;
+        let answer = 0;
+        if (type[0] === '_') answer = question[0];
+        if (type[2] === '_') answer = question[1];
+        if (type[4] === '_') answer = question[2];
+        if (buttonNumber === answer) {
+          console.log('correct');
+        } else {
+          console.log('wrong');
+        }
       }}
     >
-      {button}
+      {buttonNumber}
     </button>
   {/each}
 </section>
