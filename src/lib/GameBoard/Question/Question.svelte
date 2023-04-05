98cec2e90841050c99d7843264dda543c4877769
Z Yin <Z.Yin.MD@gmail.com>
2023-04-05 17:19:33 -0400

    fly the question to the top left on progressing to next question

--- src/lib/GameBoard/Question/Question.svelte
+++ src/lib/GameBoard/Question/Question.svelte
@@ -1,30 +1,32 @@
 <script lang="ts">
+  import { fly } from 'svelte/transition';
   import { currentQuestion } from '../../questions/$progress';
   import EmptySquare from './EmptySquare.svelte';
   $: [a, b, c] = $currentQuestion.question;
   $: type = $currentQuestion.type;
 </script>
 
-<div class="self">
-  {#if type === 'a+b=_'}
-    <code>{a} + {b} = <EmptySquare /></code>
-  {/if}
-  {#if type === 'a+_=c'}
-    <code>{a} + <EmptySquare /> = {c}</code>
-  {/if}
-  {#if type === '_+b=c'}
-    <code><EmptySquare /> + {b} = {c}</code>
-  {/if}
-  {#if type === 'c-b=_'}
-    <code>{c} - {b} = <EmptySquare /></code>
-  {/if}
-  {#if type === 'c-_=a'}
-    <code>{c} - <EmptySquare /> = {a}</code>
-  {/if}
-  {#if type === '_-b=a'}
-    <code><EmptySquare /> - {b} = {a}</code>
-  {/if}
-</div>
+{#key $currentQuestion.question}
+  <div class="self" out:fly={{ x: '-34vw', y: '-7vw', duration: 200 }}>
+    {#if type === 'a+b=_'}
+      <code>{a} + {b} = <EmptySquare /></code>
+    {/if}
+    {#if type === 'a+_=c'}
+      <code>{a} + <EmptySquare /> = {c}</code>
+    {/if}
+    {#if type === '_+b=c'}
+      <code><EmptySquare /> + {b} = {c}</code>
+    {/if}
+    {#if type === 'c-b=_'}
+      <code>{c} - {b} = <EmptySquare /></code>
+    {/if}
+    {#if type === 'c-_=a'}
+      <code>{c} - <EmptySquare /> = {a}</code>
+    {/if}
+    {#if type === '_-b=a'}
+      <code><EmptySquare /> - {b} = {a}</code>
+    {/if}
+  </div>{/key}
 
 <style>
   .self {
