5c85636a3012a191e59bd4c934724d243a7dcee5
Z Yin <Z.Yin.MD@gmail.com>
2023-04-11 15:58:50 -0400

    add animation on cloze

--- src/lib/GameBoard/Question/Cloze.svelte
+++ src/lib/GameBoard/Question/Cloze.svelte
@@ -1,17 +1,38 @@
 <script lang="ts">
   import { currentQuestionAnswer } from '../../questions/$progress';
-
-  let background = 'grey';
-  $: {
-    if (isNaN($currentQuestionAnswer.value)) background = '#eee';
-    else if ($currentQuestionAnswer.isCorrect) background = 'LightGreen';
-    else background = 'Pink';
-  }
 </script>
 
-<div class="self" style:background>{$currentQuestionAnswer.value || ''}</div>
+{#if isNaN($currentQuestionAnswer.value)}
+  <div class="self empty">{$currentQuestionAnswer.value || ''}</div>
+{:else if $currentQuestionAnswer.isCorrect}
+  <div class="self correct">{$currentQuestionAnswer.value || ''}</div>
+{:else}
+  <div class="self wrong">{$currentQuestionAnswer.value || ''}</div>
+{/if}
 
 <style>
+  @keyframes rollout {
+    0% {
+      transform: scale(1.1);
+    }
+    100% {
+      transform: none;
+    }
+  }
+  @keyframes shake {
+    0% {
+      transform: translateX(0.1em);
+    }
+    33% {
+      transform: translateX(-0.1em);
+    }
+    66% {
+      transform: translateX(0.1em);
+    }
+    100% {
+      transform: none;
+    }
+  }
   .self {
     display: grid;
     place-items: center;
@@ -22,5 +43,16 @@
     width: var(--number-button-size);
     height: var(--number-button-size);
     border-radius: var(--number-button-border-radius);
+  }
+  .empty {
+    background-color: #eee;
+  }
+  .correct {
+    background-color: lightgreen;
+    animation: rollout 0.05s;
+  }
+  .wrong {
+    background-color: pink;
+    animation: shake 0.15s;
   }
 </style>
