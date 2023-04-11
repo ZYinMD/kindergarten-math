d34cf2ca1c522136f879afc533adffed3b51ff79
Z Yin <Z.Yin.MD@gmail.com>
2023-04-10 23:24:08 -0400

    css: improve the settings view

--- src/lib/GameSettings/Settings.svelte
+++ src/lib/GameSettings/Settings.svelte
@@ -8,87 +8,98 @@
   import { defaultQuestionTypes, settings } from './$settings';
 </script>
 
-<section>
-  最大数值不超过: {$settings.maxValueAllowed}
+<div class="container">
+  <section>
+    最大数值不超过: {$settings.maxValueAllowed}
 
-  <label>
-    <input type="range" bind:value={$settings.maxValueAllowed} min="5" max="20" />
-  </label>
-</section>
-
-<section>
-  题目数量: {$settings.numQuestions}
-  <label>
-    <input type="range" bind:value={$settings.numQuestions} min="5" max="25" />
-  </label>
-</section>
-
-<section class="question-types">
-  包含题型:
-
-  {#each Object.keys(defaultQuestionTypes) as key (key)}
     <label>
-      <input type="checkbox" bind:checked={$settings.questionTypes[key]} />
-      {key}
+      <input type="range" bind:value={$settings.maxValueAllowed} min="5" max="20" />
     </label>
-  {/each}
-</section>
-<section class="hint">
-  提示:
-  <label>
-    <input
-      type="radio"
-      name="hint"
-      bind:group={$settings.allowHint}
-      value="yes"
-      disabled={$settings.maxValueAllowed > 20}
-    />
-    始终显示
-  </label>
+  </section>
 
-  <label>
-    <input
-      type="radio"
-      name="hint"
-      bind:group={$settings.allowHint}
-      value="onClick"
-      disabled={$settings.maxValueAllowed > 20}
-    />
-    点击后显示
-  </label>
+  <section>
+    题目数量: {$settings.numQuestions}
+    <label>
+      <input type="range" bind:value={$settings.numQuestions} min="5" max="25" />
+    </label>
+  </section>
 
-  <label>
-    <input
-      type="radio"
-      name="hint"
-      bind:group={$settings.allowHint}
-      value="no"
-      disabled={$settings.maxValueAllowed > 20}
-    />
-    不显示
-  </label>
-</section>
+  <section>
+    允许题型:
+    <div class="question-types">
+      {#each Object.keys(defaultQuestionTypes) as key (key)}
+        <label>
+          <input type="checkbox" bind:checked={$settings.questionTypes[key]} />
+          {key}
+        </label>
+      {/each}
+    </div>
+  </section>
+  <section class="hint">
+    提示:
+    <label>
+      <input
+        type="radio"
+        name="hint"
+        bind:group={$settings.allowHint}
+        value="yes"
+        disabled={$settings.maxValueAllowed > 20}
+      />
+      始终显示
+    </label>
 
-<section>
-  <button
-    on:click={() => {
-      questionUtil.newGame();
-      currentView.set('questions');
-    }}
-  >
-    开始
-  </button>
-</section>
+    <label>
+      <input
+        type="radio"
+        name="hint"
+        bind:group={$settings.allowHint}
+        value="onClick"
+        disabled={$settings.maxValueAllowed > 20}
+      />
+      点击后显示
+    </label>
 
-<section class="app-version">
-  <!-- svelte-ignore missing-declaration -->
-  app version: {__version__}
-</section>
+    <label>
+      <input
+        type="radio"
+        name="hint"
+        bind:group={$settings.allowHint}
+        value="no"
+        disabled={$settings.maxValueAllowed > 20}
+      />
+      不显示
+    </label>
+  </section>
+
+  <section>
+    <button
+      on:click={() => {
+        questionUtil.newGame();
+        currentView.set('questions');
+      }}
+    >
+      开始
+    </button>
+  </section>
+
+  <section class="app-version">
+    <!-- svelte-ignore missing-declaration -->
+    app version: {__version__}
+  </section>
+</div>
 
 <style>
+  .container {
+    padding: 4vw;
+    font-size: 2vw;
+  }
+  section {
+    margin-bottom: 1em;
+  }
   .question-types {
-    display: flex;
-    flex-direction: column;
+    margin: 0.5rem 0;
+    display: grid;
+    grid-template-columns: repeat(3, 13vw);
   }
   .app-version {
     color: #ddd;
